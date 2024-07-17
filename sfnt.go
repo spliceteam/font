9187c406532a76b5e3ba2725c815d7c173b7cbd3
Dominik Madarász <zaklaus@madaraszd.net>
2024-07-17 10:41:23 +0200

    Disable SFNT checksum check

--- sfnt.go
+++ sfnt.go
@@ -275,16 +275,16 @@ func parseSFNT(b []byte, index int, embedded bool) (*SFNT, error) {
 			}
 
 			// to check checksum for head table, replace the overal checksum with zero and reset it at the end
-			checksumAdjustment := binary.BigEndian.Uint32(b[offset+8:])
-			binary.BigEndian.PutUint32(b[offset+8:], 0x00000000)
-			if calcChecksum(b[offset:offset+length+padding]) != checksum {
-				return nil, fmt.Errorf("%s: bad checksum", tag)
-			} else if 0xB1B0AFBA-calcChecksum(b) != checksumAdjustment {
-				return nil, fmt.Errorf("bad checksum")
-			}
-			binary.BigEndian.PutUint32(b[offset+8:], checksumAdjustment)
+			// checksumAdjustment := binary.BigEndian.Uint32(b[offset+8:])
+			// binary.BigEndian.PutUint32(b[offset+8:], 0x00000000)
+			// if calcChecksum(b[offset:offset+length+padding]) != checksum {
+			// 	return nil, fmt.Errorf("%s: bad checksum", tag)
+			// } else if 0xB1B0AFBA-calcChecksum(b) != checksumAdjustment {
+			// 	return nil, fmt.Errorf("bad checksum")
+			// }
+			// binary.BigEndian.PutUint32(b[offset+8:], checksumAdjustment)
 		} else if calcChecksum(b[offset:offset+length+padding]) != checksum {
-			return nil, fmt.Errorf("%s: bad checksum", tag)
+			// return nil, fmt.Errorf("%s: bad checksum", tag)
 		}
 		tables[tag] = b[offset : offset+length : offset+length]
 	}
